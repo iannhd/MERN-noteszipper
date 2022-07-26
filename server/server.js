73998a8e4fa7ff6e3e6baa71b5058360d6c2cb9a
iannhd <in.hadyan@gmail.com>
2022-07-26 15:19:31 +0700

    first push

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const dotenv = require('dotenv')
+const express = require('express')
+const app = express()
+dotenv.config()
+const notes = require('./data/notes')
+
+
+app.get('/', (req, res)=>{
+    res.send("API is running...")
+})
+
+app.get('/api/notes', (req, res)=>{
+    res.json(notes)
+})
+
+app.get('/api/notes/:id', (req,res) => {
+    const note = notes.find((n)=> n._id === req.params.id)
+
+    res.send(note)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running on ${PORT}`))
